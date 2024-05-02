faa1f8fd83646b77ef27b2be9d000833deebd5d2
MONLAU\himelmaj <himelmajumder1@gmail.com>
2024-05-02 17:04:49 +0200

    Agregar campo de categoría en el modelo de productos

--- backend/server/models.py
+++ backend/server/models.py
@@ -1,11 +1,18 @@
 from typing import List, Optional
 from sqlmodel import Field, Relationship, Session, SQLModel
 from datetime import date, datetime
+from enum import Enum
+
+class Category(str, Enum):
+    man = 'man'
+    woman = 'woman'
+    unisex = 'unisex'
 
 class ProductBase(SQLModel):
     name: str = Field(max_length=100)
     description: Optional[str] = Field(default=None)
     price: float = Field(default=0.0)
+    category: Category = Field(default=None)
     image: Optional[str] =  Field(default=None)
     created_at: Optional[datetime] = Field(default=datetime.now())
     updated_at: Optional[datetime] = Field(default=datetime.now())
