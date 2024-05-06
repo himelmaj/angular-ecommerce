e918a5ab5c17d0e48f5b5e91943be17f9c17e433
MONLAU\himelmaj <himelmajumder1@gmail.com>
2024-05-06 18:51:32 +0200

    ✨update endpoint

--- backend/server/routes.py
+++ backend/server/routes.py
@@ -10,18 +10,18 @@ from .services import get_all_products_or_404, create_product_or_404, update_pro
 router = APIRouter()
 
 
-@router.get('/product', response_model=List[ProductRead], status_code=status.HTTP_200_OK)
+@router.get('/products', response_model=List[ProductRead], status_code=status.HTTP_200_OK)
 def get_products(*, session: Session = Depends(get_session)):
     db_products = get_all_products_or_404(session)
     return db_products
 
 
-@router.post('/product', response_model=ProductRead, status_code=status.HTTP_201_CREATED)
+@router.post('/products', response_model=ProductRead, status_code=status.HTTP_201_CREATED)
 def create_product(*, product: ProductCreate, session: Session = Depends(get_session)):
     db_product = create_product_or_404(session, product)
     return db_product
 
-@router.patch('/product/{product_id}', response_model=ProductRead, status_code=status.HTTP_200_OK)
+@router.patch('/products/{product_id}', response_model=ProductRead, status_code=status.HTTP_200_OK)
 def update_product(*, product_id: int, product: ProductUpdate, session: Session = Depends(get_session)):
     db_product = update_product_or_404(session, product_id, product)
     return db_product
